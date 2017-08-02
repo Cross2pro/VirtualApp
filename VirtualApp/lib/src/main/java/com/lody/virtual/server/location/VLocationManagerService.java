ab5cd49eee23fcd917b8636ca2baf53c25954e8e
keyongyu <keyongyu@ifunbow.com>
2017-08-02 10:04:19 +0800

    屏幕开/关，超时控制位置信息发送

--- VirtualApp/lib/src/main/java/com/lody/virtual/server/location/VLocationManagerService.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/server/location/VLocationManagerService.java
@@ -1,7 +1,10 @@
 package com.lody.virtual.server.location;
 
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
@@ -30,10 +33,17 @@ public class VLocationManagerService extends ILocationManager.Stub {
     private final List<IGpsStatusListener> mGpsStatusListeners = new ArrayList<>();
     private final List<ILocationListener> mLocationListeners = new ArrayList<>();
     private final static boolean DEBUG = false;
+    //屏幕关闭不发送位置信息
+    private final static boolean CONFIG_SCREEN = true;
+    //每次定位的发送信息有效时间
+    private final static boolean CONFIG_LOCATION_TIME = true;
+
     private final static int MSG_HANDLE_LOCATION = 1;
     private long mLastGPS, mLastLocation;
+    private long mHandleStartTime;
     private static final boolean REPORT_LOCATION_WITH_GPS_STATUS = false;
     final Random mRandom = new Random(System.currentTimeMillis());
+
     private final static int HANDLE_TIME_GPS = 5 * 1000;
     /**
      * 报告位置时间间隔
@@ -43,6 +53,13 @@ public class VLocationManagerService extends ILocationManager.Stub {
      * 多少时间报告一次
      */
     private final static int HANDLE_TIME_LOCATION = 30 * 1000;
+    /***
+     * 发送定位信息，多少时间后停止，为了减少没必要的浪费
+     */
+    private final static long HANDLE_TIME_MAX = 60 * 60 * 1000;
+
+    //屏幕关闭的时候不发送位置
+    private boolean mScreeLock = false;
 
     private final HandlerThread mHandlerThread;
 
@@ -55,6 +72,9 @@ public class VLocationManagerService extends ILocationManager.Stub {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case MSG_HANDLE_LOCATION:
+                    if (mScreeLock) {
+                        return;
+                    }
                     handLocationChanged(false, false);
                     startHandleTask();
                     return;
@@ -80,6 +100,23 @@ public class VLocationManagerService extends ILocationManager.Stub {
         mHandlerThread = new HandlerThread("location_work");
         mHandlerThread.start();
         mHandler = new WorkHandler(mHandlerThread.getLooper());
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        if(CONFIG_SCREEN) {
+            context.getApplicationContext().registerReceiver(new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    //mScreeLock
+                    if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+                        mScreeLock = true;
+                    } else if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
+                        mScreeLock = false;
+                    }
+                }
+            }, intentFilter);
+        }else{
+            mScreeLock = false;
+        }
     }
 
     @Override
@@ -157,6 +194,7 @@ public class VLocationManagerService extends ILocationManager.Stub {
             }
             mLocationListeners.add(listener);
         }
+        mHandleStartTime = System.currentTimeMillis();
         handLocationChanged(true, false);
         startHandleTask();
     }
@@ -179,8 +217,14 @@ public class VLocationManagerService extends ILocationManager.Stub {
     }
 
     private void startHandleTask() {
+        if (mScreeLock) return;
         synchronized (mLocationListeners) {
             if (mLocationListeners.size() == 0) {
+                return;
+            }
+        }
+        if(CONFIG_LOCATION_TIME) {
+            if (System.currentTimeMillis() - mHandleStartTime >= HANDLE_TIME_MAX) {
                 return;
             }
         }
@@ -197,7 +241,8 @@ public class VLocationManagerService extends ILocationManager.Stub {
 //        } else {
 //            mLastGPS = System.currentTimeMillis();
 //        }
-        if(REPORT_LOCATION_WITH_GPS_STATUS) {
+        if (mScreeLock) return;
+        if (REPORT_LOCATION_WITH_GPS_STATUS) {
             synchronized (mGpsStatusListeners) {
                 for (int i = mGpsStatusListeners.size() - 1; i >= 0; i--) {
                     IGpsStatusListener listener = mGpsStatusListeners.get(i);
