3e6a73dc813140ce9398664cd68e9eb6b7989ac1
keyongyu <247321453@qq.com>
2017-12-09 17:30:15 +0800

    remove only read

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/VClientImpl.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/VClientImpl.java
@@ -416,7 +416,6 @@ public final class VClientImpl extends IVClient.Stub {
         NativeEngine.redirectDirectory("/data/data/" + info.packageName + "/lib/", libPath);
         NativeEngine.redirectDirectory("/data/user/0/" + info.packageName + "/lib/", libPath);
 
-        NativeEngine.readOnly(VEnvironment.getDataAppDirectory().getPath());
         VirtualStorageManager vsManager = VirtualStorageManager.get();
         String vsPath = vsManager.getVirtualStorage(info.packageName, userId);
         boolean enable = vsManager.isVirtualStorageEnable(info.packageName, userId);
