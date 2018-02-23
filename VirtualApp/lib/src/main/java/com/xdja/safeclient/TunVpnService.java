675fa10a2cc5e435ba7fc08fab60e3495693a1dd
lxf <lxf@xdja.com>
2018-02-23 16:40:52 +0800

    add TunVpnService file

---
+++ VirtualApp/lib/src/main/java/com/xdja/safeclient/TunVpnService.java
@@ -0,0 +1,11 @@
+package com.xdja.safeclient;
+
+import android.net.VpnService;
+
+/**
+ * Created by wxudong on 17-11-15.
+ */
+
+public class TunVpnService extends VpnService{
+    //add com.xdja.safeclient.TunVpnService into AndroidManifest.xml
+}
