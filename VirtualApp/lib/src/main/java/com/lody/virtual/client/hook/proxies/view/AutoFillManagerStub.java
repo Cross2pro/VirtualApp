95d84c3eae717efb21fdce5d7ebfc49302317c52
lxf <lxf@xdja.com>
2018-09-12 17:59:31 +0800

    AutoFillManager 中startSession 替换packagename失败问题。

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/proxies/view/AutoFillManagerStub.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/proxies/view/AutoFillManagerStub.java
@@ -1,11 +1,12 @@
 package com.lody.virtual.client.hook.proxies.view;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.util.Log;
 
 import com.lody.virtual.client.hook.base.BinderInvocationProxy;
-import com.lody.virtual.client.hook.base.MethodProxy;
-import com.lody.virtual.client.hook.utils.MethodParameterUtils;
+import com.lody.virtual.client.hook.base.ReplaceLastPkgMethodProxy;
+import com.lody.virtual.helper.utils.ArrayUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -21,6 +22,7 @@ public class AutoFillManagerStub extends BinderInvocationProxy {
     private static final String TAG = "AutoFillManagerStub";
 
     private static final String AUTO_FILL_NAME = "autofill";
+
     public AutoFillManagerStub() {
         super(IAutoFillManager.Stub.asInterface, AUTO_FILL_NAME);
     }
@@ -45,39 +47,31 @@ public class AutoFillManagerStub extends BinderInvocationProxy {
             Log.e(TAG, "AutoFillManagerStub inject error.", tr);
             return;
         }
-        addMethodProxy(new MethodProxy() {
-            @Override
-            public String getMethodName() {
-                return "startSession";
-            }
-            @Override
-            public boolean beforeCall(Object who, Method method, Object... args) {
-                MethodParameterUtils.replaceLastAppPkg(args);
-                return super.beforeCall(who, method, args);
-            }
-        });
-        addMethodProxy(new MethodProxy() {
-            @Override
-            public String getMethodName() {
-                return "updateOrRestartSession";
-            }
-            @Override
-            public boolean beforeCall(Object who, Method method, Object... args) {
-                MethodParameterUtils.replaceLastAppPkg(args);
-                return super.beforeCall(who, method, args);
-            }
-        });
-        addMethodProxy(new MethodProxy() {
-            @Override
-            public String getMethodName() {
-                return "isServiceEnabled";
-            }
-            @Override
-            public boolean beforeCall(Object who, Method method, Object... args) {
-                MethodParameterUtils.replaceLastAppPkg(args);
-                return super.beforeCall(who, method, args);
-            }
-        });
+
+        addMethodProxy(new ReplacePkgAndComponentProxy("startSession"));
+        addMethodProxy(new ReplacePkgAndComponentProxy("updateOrRestartSession"));
+        addMethodProxy(new ReplaceLastPkgMethodProxy("isServiceEnabled"));
     }
 
+    static class ReplacePkgAndComponentProxy extends ReplaceLastPkgMethodProxy {
+
+        ReplacePkgAndComponentProxy(String name) {
+            super(name);
+        }
+
+        @Override
+        public boolean beforeCall(Object who, Method method, Object... args) {
+            replaceLastAppComponent(args, getHostPkg());
+            return super.beforeCall(who, method, args);
+        }
+
+        private void replaceLastAppComponent(Object[] args, String hostPkg) {
+            int index = ArrayUtils.indexOfLast(args, ComponentName.class);
+            if (index != -1) {
+                ComponentName orig = (ComponentName) args[index];
+                ComponentName newComponent = new ComponentName(hostPkg, orig.getClassName());
+                args[index] = newComponent;
+            }
+        }
+    }
 }
