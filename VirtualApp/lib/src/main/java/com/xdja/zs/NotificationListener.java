dad79d38197301e72c6f654dcf629cdef1975886
zhangsong@w541 <duoniduoni@126.com>
2019-05-08 17:39:36 +0800

    对于盒外通知栏的处理：   如果在工作空间，所有系统APP弹出的通知栏都删掉~

--- VirtualApp/lib/src/main/java/com/xdja/zs/NotificationListener.java
+++ VirtualApp/lib/src/main/java/com/xdja/zs/NotificationListener.java
@@ -2,7 +2,11 @@ package com.xdja.zs;
 
 import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
@@ -17,11 +21,39 @@ public class NotificationListener extends NotificationListenerService {
     public NotificationListener() {
     }
 
+    private Context mApp = null;
+    private PackageManager packageManager = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mApp = getApplicationContext();
+        packageManager = mApp.getPackageManager();
+    }
+
     @Override
     public void onListenerConnected() {
         super.onListenerConnected();
 
         Log.e(Tag, "onListenerConnected");
+    }
+
+    private boolean isSystemApp(String pkgName)
+    {
+        boolean ret = false;
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("isThirdApp: ");
+            stringBuilder.append(pkgName);
+            Log.d(Tag, stringBuilder.toString());
+            if ((packageManager.getPackageInfo(pkgName, 0).applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                ret = true;
+            }
+            return ret;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
@@ -40,11 +72,32 @@ public class NotificationListener extends NotificationListenerService {
         Log.e(Tag, String.format("onNotificationPosted [Title: %s] [Text: %s] [SubText: %s] [pkgName %s]",
                 notificationTitle, notificationText, notificationSubText, packageName));
 
-        if(packageName.equals("com.android.server.telecom") && ActivityCounterManager.get().isForeGround())
+        if(mApp.getPackageName().equals(packageName))
         {
-            Log.e(Tag, "delete this notify " + sbn.getKey());
-            cancelNotification(sbn.getKey());
-            //snoozeNotification(sbn.getKey(), 10 * 1000);
+            Log.e(Tag, String.format("\tthis notify cames from inside, ignore"));
+
+            return;
         }
+
+        if(isSystemApp(packageName) && currentSpace())
+        {
+            Log.e(Tag, "\tdelete this notify " + sbn.getKey());
+            cancelNotification(sbn.getKey());
+        }
+    }
+
+    boolean currentSpace() {
+        Context context = this;
+        Uri CONTENT_URI = Uri.parse("content://com.xdja.engine.provider");
+
+        try {
+            Bundle bundle = new Bundle();
+            Bundle result = context.getContentResolver().call(CONTENT_URI, "currentSpace", null, bundle);
+            return result.getBoolean("space");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
