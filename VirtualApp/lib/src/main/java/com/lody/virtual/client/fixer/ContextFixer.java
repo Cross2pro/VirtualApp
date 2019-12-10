4b7809ee22d0283cd81f1586df27a2deb17cb91a
keyongyu <keyongyu@xdja.com>
2019-12-10 13:53:17 +0800

    fix unsafe's application

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/fixer/ContextFixer.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/fixer/ContextFixer.java
@@ -2,6 +2,7 @@ package com.lody.virtual.client.fixer;
 
 import android.app.AlarmManager;
 import android.app.AppOpsManager;
+import android.app.Application;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.net.wifi.WifiManager;
@@ -10,6 +11,7 @@ import android.os.Build;
 import android.os.DropBoxManager;
 import android.os.IInterface;
 import android.os.health.SystemHealthManager;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.lody.virtual.client.VClient;
@@ -30,8 +32,10 @@ import com.lody.virtual.helper.utils.VLog;
 
 import java.util.concurrent.Executor;
 
+import mirror.android.app.ActivityThread;
 import mirror.android.app.ContextImpl;
 import mirror.android.app.ContextImplKitkat;
+import mirror.android.app.LoadedApk;
 import mirror.android.content.ContentResolverJBMR2;
 
 /**
@@ -68,8 +72,23 @@ public class ContextFixer {
         if (!VirtualCore.get().isVAppProcess()) {
             return;
         }
-
         String hostPkg = VirtualCore.get().getHostPkg();
+        if (LoadedApk.mApplication != null) {
+            //fix LoadedApk's application
+            Object info = ContextImpl.mPackageInfo.get(context);
+            if (info != null) {
+                Application application = LoadedApk.mApplication.get(info);
+                if (application != null && hostPkg.equals(application.getPackageName())) {
+                    //
+                    VLog.w("kk", "application is host!!");
+                    Application app = VClient.get().getCurrentApplication();
+                    if(app != null) {
+                        LoadedApk.mApplication.set(info, app);
+                    }
+                }
+            }
+        }
+
         ContextImpl.mBasePackageName.set(context, hostPkg);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             ContextImplKitkat.mOpPackageName.set(context, hostPkg);
